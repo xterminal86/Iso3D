0eef1b5b4d2cba3316abd58c9f9dfff10148eaf1
Nickolay <xterminal86@gmail.com>
2017-12-08 19:14:24 +0300

    music loop

--- Assets/scripts/test/MusicLoopTest.cs
+++ Assets/scripts/test/MusicLoopTest.cs
@@ -6,22 +6,22 @@ public class MusicLoopTest : MonoBehaviour
 {
   public AudioSource MusicTrack;
 
-  double _sampleRate = 0.0f;
-  double _loopStart = 0.0f;
-	void Start () 
+	void Start() 
   {
-    _sampleRate = AudioSettings.outputSampleRate;
-    _loopStart = 326663 / _sampleRate;
-    GameObject loopSegment = new GameObject();
+    // To create endless loop first we play original track until the end
+    // and then play endlessly fragment of the original track that needs to be looped.
+    // We should use PlayScheduled for it uses audio system time (dspTime) thus making 
+    // all track switching/manipulation framerate independent.
+
+    GameObject loopSegment = new GameObject("Loop Fragment");
     AudioSource loop = loopSegment.AddComponent<AudioSource>();
     loop.playOnAwake = false;
-    loop.clip = MusicTrack.clip;
-    loop.timeSamples = 326663;
+    float[] samples = new float[(MusicTrack.clip.samples - 326663) * MusicTrack.clip.channels];
+    MusicTrack.clip.GetData(samples, 326663);
+    loop.clip = AudioClip.Create(MusicTrack.clip.name + "-loop", MusicTrack.clip.samples - 326663, MusicTrack.clip.channels, MusicTrack.clip.frequency, false);
+    loop.clip.SetData(samples, 0);
     loop.loop = true;
     loop.PlayScheduled(AudioSettings.dspTime + MusicTrack.clip.length);
     MusicTrack.Play();
-    //Debug.Log(_loopStart);
-    //MusicTrack.PlayScheduled(_loopStart);
-    //MusicTrack.SetScheduledStartTime(_loopStart);
 	}
 }
