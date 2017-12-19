ecb2c948912903ff45a8b1a53967733f8c6e348d
xterminal86 <xterminal86@gmail.com>
2017-12-19 22:55:21 +0300

    small name gen addition

--- Assets/scripts/extra/NameGen.cs
+++ Assets/scripts/extra/NameGen.cs
@@ -13,7 +13,7 @@ public class NameGen : MonoBehaviour
 
   List<string> _endings = new List<string>()
   {
-    "IA", "YA", "ND", "NT", "SH", "RSH", "EY", "URGH", "URG"
+    "IA", "YA", "ND", "NT", "SH", "RSH", "EY", "URGH", "URG", "STAN"
   };
 
   void Awake()
