8c79b8438233407897008e2eee1ceb973dee4ef2
xterminal86 <xterminal86@gmail.com>
2018-01-10 02:53:20 +0300

    attack meter test scene

---
+++ Assets/scripts/core/singletons/GameTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTime : MonoSingleton<GameTime> 
+{
+  float _timer = 0.0f;
+  public void SetTimer(float seconds, Callback cb)
+  {
+    _timer = seconds;
+  }
+
+  void Update()
+  {
+    if (_timer > 0.0f)
+    {
+      _timer -= Time.deltaTime;
+    }
+  }
+}
---
+++ Assets/scripts/test/AttackMeter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackMeter : MonoBehaviour 
+{
+  public Image AttackMeterImage;
+
+  float _meterMaxWidth = 980.0f;
+
+  public int Speed = 100;
+
+  float _speedMeter = 0.0f;
+  float _speedToReach = 0.0f;
+  float _meterFillFraction = 0.0f;
+  void Start()
+  {
+    // Assuming Speed = 100 -> 5 seconds
+    _meterFillFraction = (float)Speed / 100.0f;
+    _speedToReach = 5.0f / _meterFillFraction;
+
+    InvokeRepeating("FillMeter", 0.0f, _meterFillFraction); 
+  }
+
+  void FillMeter()
+  {
+    if (_speedMeter >= _speedToReach)
+    {
+      Debug.Log("reached");
+      return;
+    }
+
+    _speedMeter += _meterFillFraction;
+
+    Debug.Log(_speedMeter);
+  }
+
+  bool _isPaused = false;
+  void Update()
+  {
+    if (!_isPaused && Input.GetMouseButtonDown(0))
+    {
+      Debug.Log("paused");
+      _isPaused = true;
+      CancelInvoke();
+    }
+    else if (_isPaused && Input.GetMouseButtonDown(1))      
+    {
+      _isPaused = false;
+      InvokeRepeating("FillMeter", 0.0f, _meterFillFraction); 
+    }
+  }
+}
