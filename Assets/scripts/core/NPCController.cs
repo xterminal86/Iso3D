bd24696473f65e1d3e2798cfafa766cd98667eec
Nickolay <xterminal86@gmail.com>
2018-02-08 19:39:56 +0300

    radius marker

---
+++ Assets/scripts/core/NPCController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour 
+{
+  public PlayerInZoneDetector PlayerDetector;
+
+  void Awake()
+  {
+    PlayerDetector.MethodToCallOnEnter += PlayerDetected;
+    PlayerDetector.MethodToCallOnExit += PlayerExited;
+  }
+
+  void PlayerDetected(Collider c)
+  {
+    Debug.Log("detected " + c);
+  }
+
+  void PlayerExited(Collider c)
+  {
+    Debug.Log("exited " + c);
+  }
+}
