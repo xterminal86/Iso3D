5e050700f9d6c20099a77e452e2ae5fe21fd34af
xterminal86 <xterminal86@gmail.com>
2018-04-09 20:17:33 +0300

    lever model

--- Assets/scripts/world-building/hierarchy/WorldObjectBase.cs
+++ Assets/scripts/world-building/hierarchy/WorldObjectBase.cs
@@ -42,4 +42,51 @@ public class WorldObjectBase : MonoBehaviour
   public virtual void PostProcess()
   {    
   }
+
+  /// <summary>
+  /// Handles monologues of characters when they are looking at object
+  /// </summary>
+  /// <param name="inspectingActor">Inspecting actor</param>
+  public virtual void Inspect(ActorStats inspectingActor)
+  {
+  }
+
+  /// <summary>
+  /// Speak with "object" by selecting actor or topic in FormTalk
+  /// </summary>
+  /// <param name="actor">Actor who initiated dialog or null if topic is used</param>
+  /// <param name="topic">Topic of conversation selected in FormTalk or null if actor was selected</param>
+  public virtual void Speak(ActorStats actor, string topic)
+  {
+  }
+
+  /// <summary>
+  /// Interact with object using hand (e.g. pull lever)
+  /// </summary>
+  public virtual void Interact()
+  {
+    foreach (var item in _interactions)
+    {
+      if (item != null)
+        item();
+    }
+  }
+
+  // Controller can control multiple objects (e.g. one lever opens more than one door)
+  List<Callback> _interactions = new List<Callback>();
+
+  /// <summary>
+  /// _interactions are subscribed on this method
+  /// </summary>
+  public virtual void InteractHandler()
+  {
+  }
+
+  /// <summary>
+  /// Interact with object using some other object (e.g. insert cog into mechanism)
+  /// </summary>
+  /// <param name="obj">Object to use upon this one</param>
+  public virtual void InteractWithObject(WorldObjectBase obj)
+  {
+  }
 }
