27abccc6b9d46e9b594068c0cb6ded56238d7219
xterminal86 <xterminal86@gmail.com>
2018-04-12 19:30:45 +0300

    -
    TextMeshPro
    Stats growth rates test scene

--- Assets/scripts/core/actors-logic/ActorStats.cs
+++ Assets/scripts/core/actors-logic/ActorStats.cs
@@ -11,14 +11,74 @@ public class ActorStats : ScriptableObject
   public Int2 Strength = new Int2();
   public Int2 Defence = new Int2();
   public Int2 Speed = new Int2();
+  public Int2 Affinity = new Int2();
+
+  [Header("Growth Rates (%)")]    
+  public int StrengthGrowth = 0;
+  public int DefenceGrowth = 0;
+  public int SpeedGrowth = 0;
+  public int AffinityGrowth = 0;
+
+  [Header("")]    
+  public int Experience = 0;
+  public int Level = 1;
 
   public Int2 Hitpoints = new Int2();
-  public Int2 UnityPoints = new Int2();
 
   public string CharName = "Unnamed Actor";
 
   public override string ToString()
   {
-    return string.Format("[ActorStats] = Name: {5} STR: {0} DEF: {1} SPD: {2} HP: {3} UP: {4}", Strength, Defence, Speed, Hitpoints, UnityPoints, CharName);
+    return string.Format("[ActorStats] = Name: {5} STR: {0} DEF: {1} SPD: {2} HP: {3} UP: {4}", Strength, Defence, Speed, Hitpoints, Affinity, CharName);
   }
+
+  public void Reset()
+  {
+    Strength.X = Strength.Y;
+    Defence.X = Defence.Y;
+    Speed.X = Speed.Y;
+    Affinity.X = Affinity.Y;
+    Hitpoints.X = Hitpoints.Y;
+    Experience = 0;
+    Level = 1;
+  }
+
+  List<int> _growResults = new List<int>();
+  public List<int> LevelUp()
+  {
+    _growResults.Clear();
+
+    int str = RollStat(StrengthGrowth);
+    int def = RollStat(DefenceGrowth);
+    int spd = RollStat(SpeedGrowth);
+    int afy = RollStat(AffinityGrowth);
+
+    _growResults.Add(str);
+    _growResults.Add(def);
+    _growResults.Add(spd);
+    _growResults.Add(afy);
+
+    Strength.X += str;
+    Defence.X += def;
+    Speed.X += spd;
+    Affinity.X += afy;
+
+    Experience = 0;
+    Level++;
+
+    return _growResults;
+  }
+
+  int RollStat(int growthRate)
+  {
+    int increment = 0;
+
+    int roll = UnityEngine.Random.Range(0, 101);
+    if (roll <= growthRate)
+    {
+      increment = 1;
+    }
+
+    return increment;
+  }  
 }
---
+++ Assets/scripts/test/LevelupTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelupTest : MonoBehaviour 
+{
+  public ActorStats ActorStatsRef;
+  public TMP_Text TextComponent;
+  public TMP_Text IncrementsText;
+  public Button LevelUpButton;
+
+  string _text = string.Empty;
+  void Start()
+  {
+    UpdateText();
+  }
+
+  void UpdateText()
+  {
+    _text = string.Format("{0}\n\nSTR: {1}\nDEF: {2}\nSPD: {3}\nAFY: {4}\n\n", ActorStatsRef.CharName, ActorStatsRef.Strength.X, ActorStatsRef.Defence.X, ActorStatsRef.Speed.X, ActorStatsRef.Affinity.X);
+    _text += string.Format("EXP: {0}\nLVL: {1}", ActorStatsRef.Experience, ActorStatsRef.Level);
+    TextComponent.text = _text;
+  }
+
+  bool _working = false;
+  public void LevelUpHandler()
+  {
+    if (!_working)
+    {
+      StartCoroutine(GainExpRoutine());
+      _working = true;
+    }
+  }
+
+  IEnumerator GainExpRoutine()
+  {
+    int exp = 0;
+    while (exp < 100)
+    {
+      exp++;
+      ActorStatsRef.Experience = exp;
+
+      UpdateText();
+
+      yield return null;
+    }
+
+    _working = false;
+
+    var increments = ActorStatsRef.LevelUp();
+
+    string incrementsText = "\n\n";
+    foreach (var item in increments)
+    {
+      incrementsText += string.Format("+{0}\n", item);
+    }
+
+    IncrementsText.text = incrementsText;
+
+    UpdateText();
+
+    yield return null;
+  }
+
+  void OnDisable()
+  {
+    ActorStatsRef.Reset();
+  }
+}
